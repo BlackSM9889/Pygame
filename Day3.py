86853abdbe4b3a4640aa44dee6b26f1fc0d446fb
BlackSM9889 <royboy9889@gmail.com>
2026-04-15 02:47:25 +0800

    2026-4-14 角色圖片輸入 邊界設定 跳躍出現問題(明天想辦法解決

--- Day3.py
+++ Day3.py
@@ -2,6 +2,7 @@ import pygame
 from sys import exit
 import os
 
+#變數
 GAME_WIDTH = 1280
 GAME_HEIGHT = 720
 
@@ -11,6 +12,11 @@ PLAYER_WIDTH = 64
 PLAYER_HEIGHT = 96
 PLAYER_DISTANCE = 5
 
+GRAVITY = 0.5
+PLAYER_VELOCITY_Y = -10
+FLOOR_Y = GAME_HEIGHT * 3/4
+
+#image
 background_image = pygame.image.load(os.path.join("image", "background.png"))
 player_image_right = pygame.image.load(os.path.join("image", "robot-right.png"))
 player_image_right = pygame.transform.scale(player_image_right, (PLAYER_WIDTH, PLAYER_HEIGHT))
@@ -27,6 +33,15 @@ class Player(pygame.Rect):
 
 player = Player()
 
+def move():
+    player.velocity_y += GRAVITY
+    player.y += player.velocity_y
+
+    if player.y + player.height > FLOOR_Y:
+        player.y = FLOOR_Y - player.height
+        player.jumping = False
+
+
 def draw():
     window.fill("black")
     scaled_background = pygame.transform.scale(background_image, (GAME_WIDTH, GAME_HEIGHT))
@@ -41,19 +56,19 @@ while True:
             exit()
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP] or keys[pygame.K_w]:
-        player.y = max(player.y - PLAYER_DISTANCE, 0)
-       
-    if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and player.y + player.height + PLAYER_DISTANCE <= GAME_HEIGHT:
-        player.y = min(player.y + PLAYER_DISTANCE, GAME_HEIGHT - player.height)
+    if (keys[pygame.K_UP] or keys[pygame.K_w]) and not player.jumping:
+        player.velocity_y = PLAYER_VELOCITY_Y
+        player.jumping = True
 
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
         player.x = max(player.x - PLAYER_DISTANCE, 0)
+        player.direction = "left"
 
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         player.x = min(player.x + PLAYER_DISTANCE, GAME_WIDTH - player.width)
+        player.direction = "right"
         
     draw()    
-
+    move()
     pygame.display.update()
     clock.tick(60)
